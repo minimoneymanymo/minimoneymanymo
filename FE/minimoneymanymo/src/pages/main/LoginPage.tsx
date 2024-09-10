db552066bd2e9b8a0bf4cbb862def6394c05d90a
이창현 <96161882+changhyeon12333@users.noreply.github.com>
2024-09-10 11:16:59 +0900

    test : conflict

--- FE/minimoneymanymo/src/pages/main/LoginPage.tsx
+++ FE/minimoneymanymo/src/pages/main/LoginPage.tsx
@@ -1,5 +1,12 @@
 function LoginPage(): JSX.Element {
-  return <>LoginPage</>
+  return (
+    <div className=" w-full ">
+      <div className="flex justify-center items-center w-fit min-h-screen mx-auto">
+        
+      </div>
+    </div>
+  );
 }
+
 
 export default LoginPage
--- FE/minimoneymanymo/src/pages/main/SignUpPage.tsx
+++ FE/minimoneymanymo/src/pages/main/SignUpPage.tsx
@@ -1,5 +1,16 @@
+
+import SimpleRegistrationForm from "@/components/auth/SimpleRegistrationForm";
+
+
+
 function SignUpPage(): JSX.Element {
-  return <>SignUpPage</>
+  return (
+    <div className=" w-full ">
+      <div className="flex justify-center items-center w-fit min-h-screen mx-auto">
+        <SimpleRegistrationForm></SimpleRegistrationForm>
+      </div>
+    </div>
+  );
 }
 
 export default SignUpPage
