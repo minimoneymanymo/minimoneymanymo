e4db017d4d42a43f16f92eb5f609a56ba705c0ba
조아름 <a_lme@naver.com>
2024-09-14 14:53:04 +0900

    feat: TradeService 수정

--- BE/mmmm/src/main/java/com/beautifulyomin/mmmm/domain/stock/service/TradeService.java
+++ BE/mmmm/src/main/java/com/beautifulyomin/mmmm/domain/stock/service/TradeService.java
@@ -1,7 +1,6 @@
 package com.beautifulyomin.mmmm.domain.stock.service;
 
 import com.beautifulyomin.mmmm.domain.stock.dto.TradeDto;
-import com.beautifulyomin.mmmm.domain.fund.entity.TradeRecord;
 
 public interface TradeService {
     void createTrade(TradeDto tradeDto);
