5b59f6e79a46c12b0621820a6ddf5e09ec586815
이창현 <96161882+changhyeon12333@users.noreply.github.com>
2024-09-15 20:21:26 +0900

    Test: buildTest 1

--- BE/mmmm/src/main/java/com/beautifulyomin/mmmm/MmmmApplication.java
+++ BE/mmmm/src/main/java/com/beautifulyomin/mmmm/MmmmApplication.java
@@ -11,10 +11,10 @@ public class MmmmApplication {
 
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
-        );
+//        Dotenv dotenv = Dotenv.configure().load();
+//        dotenv.entries().forEach(entry ->
+//                System.setProperty(entry.getKey(), entry.getValue())
+//        );
         SpringApplication.run(MmmmApplication.class, args);
     }
 }
