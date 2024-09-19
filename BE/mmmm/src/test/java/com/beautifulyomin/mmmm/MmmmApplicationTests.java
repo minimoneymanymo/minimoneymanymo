977a8c79cd37c39ba48c3bd753eb201b9ae351a9
이창현 <96161882+changhyeon12333@users.noreply.github.com>
2024-09-19 17:18:51 +0900

    fix

--- BE/mmmm/src/test/java/com/beautifulyomin/mmmm/MmmmApplicationTests.java
+++ BE/mmmm/src/test/java/com/beautifulyomin/mmmm/MmmmApplicationTests.java
@@ -3,12 +3,14 @@ package com.beautifulyomin.mmmm;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
+@EnableConfigurationProperties
 class MmmmApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
