9945d17a08b12074f919ae26caf3cd1b703faee8
조아름 <a_lme@naver.com>
2024-09-19 17:55:08 +0900

    add: TradeServiceImplTest 추가

---
+++ BE/mmmm/src/test/java/com/beautifulyomin/mmmm/domain/stock/service/TradeServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.beautifulyomin.mmmm.domain.stock.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TradeServiceImplTest {
+
+    @Test
+    void createTrade() {
+    }
+}
