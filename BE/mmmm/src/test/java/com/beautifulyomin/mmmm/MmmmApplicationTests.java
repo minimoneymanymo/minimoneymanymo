4f20d6bfb2311f4fa4380d40c057e78ead0e3d74
이창현 <96161882+changhyeon12333@users.noreply.github.com>
2024-09-20 09:57:42 +0900

    fix: 로컬에서는 빌드+실행 전부다되게 했어요

--- BE/mmmm/src/test/java/com/beautifulyomin/mmmm/MmmmApplicationTests.java
+++ BE/mmmm/src/test/java/com/beautifulyomin/mmmm/MmmmApplicationTests.java
@@ -1,8 +1,6 @@
 package com.beautifulyomin.mmmm;
-
 import io.github.cdimascio.dotenv.Dotenv;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 //@EnableConfigurationProperties
 class MmmmApplicationTests {
 
-    //@Test
     void contextLoads() {
     }
 
