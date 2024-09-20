0b7ac2db67b114a8033b1957984e018b65ad52b1
이창현 <96161882+changhyeon12333@users.noreply.github.com>
2024-09-20 13:10:49 +0900

    fix : main load

--- BE/mmmm/src/main/java/com/beautifulyomin/mmmm/MmmmApplication.java
+++ BE/mmmm/src/main/java/com/beautifulyomin/mmmm/MmmmApplication.java
@@ -1,6 +1,7 @@
 package com.beautifulyomin.mmmm;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Profile;
@@ -11,9 +12,12 @@ import java.util.Arrays;
 @SpringBootApplication
 @EnableScheduling
 public class MmmmApplication {
+    @Value("${SPRING_PROFILES_ACTIVE : devlop}")
+    private static String env;
 
     public static void main(String[] args) {
-        if (!Arrays.asList(args).contains("prod")) {
+
+        if (!env.equals("prod")) {
             Dotenv dotenv = Dotenv.configure().load();
             dotenv.entries().forEach(entry ->
                     System.setProperty(entry.getKey(), entry.getValue())
