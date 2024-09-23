ec44dd5f107be644ca8007d1088ac30fc38d8485
이다영 <da0997@naver.com>
2024-09-23 16:52:57 +0900

    feat: 투자 일기 페이지 캘린더 구현 및 url 연결

--- FE/minimoneymanymo/src/App.tsx
+++ FE/minimoneymanymo/src/App.tsx
@@ -14,6 +14,8 @@ import MyChildrenPage from "./pages/parentchildren/MyChildrenPage"
 import MyChildFinancePage from "./pages/parentchildren/MyChildFinancePage"
 import StockPageLayout from "./layouts/StockPageLayout"
 import ChartPage from "./pages/stock/ChartPage"
+import MyDairy from "./components/my-info/Calender"
+import ChildDairyPage from "./pages/chlid/ChildDairyPage"
 function App() {
   return (
     <Routes>
@@ -41,11 +43,10 @@ function App() {
           <Route path="wallet" element={<Temp />} />
           <Route path="finance" element={<Temp />} />
           <Route path="invest-style" element={<Temp />} />
-          <Route path="diary" element={<Temp />} />
+          <Route path="diary" element={<ChildDairyPage />} />
         </Route>
-        <Route path="/stock" element={<StockPageLayout/>}>
+        <Route path="/stock" element={<StockPageLayout />}>
           <Route path="detail" element={<ChartPage />} />
-        
         </Route>
       </Route>
     </Routes>
