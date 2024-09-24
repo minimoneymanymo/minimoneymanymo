d89b2de285f04605d7e890d97f3536b40897add5
이창현 <96161882+changhyeon12333@users.noreply.github.com>
2024-09-24 17:19:48 +0900

    test : time

--- BE/mmmmBatch/src/main/java/com/beautifulyomin/mmmmbatch/runner/StockJobRunner.java
+++ BE/mmmmBatch/src/main/java/com/beautifulyomin/mmmmbatch/runner/StockJobRunner.java
@@ -20,7 +20,7 @@ public class StockJobRunner {
         this.mainJob = mainJob;
     }
 
-    @Scheduled(cron = "0 5 17 ? * MON-FRI")
+    @Scheduled(cron = "0 30 17 ? * MON-FRI")
     public void run() throws Exception {
         JobParameters params = new JobParametersBuilder()
                 .addLong("time", System.currentTimeMillis())
