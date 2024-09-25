d6953cd559a6e2adeeea924e89c7a3b29bc7a893
이다영 <da0997@naver.com>
2024-09-25 15:13:07 +0900

    fix: build error 수정

--- FE/minimoneymanymo/src/pages/home2/home2.test.tsx
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest';
-import { render } from '@testing-library/react';
-import Home2 from './home2';
-
-test('renders Home2 component', () => {
-  const wrapper = render(<Home2 />);
-  expect(wrapper).toBeTruthy();
-});
