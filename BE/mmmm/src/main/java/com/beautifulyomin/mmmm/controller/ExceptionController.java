37eb4bfd60244f5ad9ea98f757f1c3084ab64689
조아름 <a_lme@naver.com>
2024-09-25 09:58:32 +0900

    feat: 에러 관련 수정

--- BE/mmmm/src/main/java/com/beautifulyomin/mmmm/controller/ExceptionController.java
+++ BE/mmmm/src/main/java/com/beautifulyomin/mmmm/controller/ExceptionController.java
@@ -5,6 +5,7 @@ import com.beautifulyomin.mmmm.domain.stock.exception.StockNotFoundException;
 import com.beautifulyomin.mmmm.exception.InvalidRequestException;
 import com.beautifulyomin.mmmm.exception.InvalidRoleException;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +15,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import javax.naming.AuthenticationException;
 import java.nio.file.AccessDeniedException;
 
+@Slf4j
 @ControllerAdvice(basePackages = "com.beautifulyomin.mmmm")
 public class ExceptionController {
 
     @ExceptionHandler({BadRequestException.class, InvalidRequestException.class})
     public ResponseEntity<CommonResponseDto> handleBadRequestException(BadRequestException ex) {
+        CommonResponseDto errorResponse = new CommonResponseDto(400, ex.getMessage(), null);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<CommonResponseDto> handleIllegalArgumentException(IllegalArgumentException ex) {
+        log.error("Handling IllegalArgumentException: {}", ex.getMessage());
         CommonResponseDto errorResponse = new CommonResponseDto(400, ex.getMessage(), null);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
@@ -43,7 +52,10 @@ public class ExceptionController {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<CommonResponseDto> handleGenericException(Exception ex) {
+        log.error("Handling Generic Exception: {}", ex.getMessage());
         CommonResponseDto errorResponse = new CommonResponseDto(500, "서버 오류: " + ex.getMessage(), null);
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+
 }
--- BE/mmmm/src/main/java/com/beautifulyomin/mmmm/domain/stock/service/TradeServiceImpl.java
+++ BE/mmmm/src/main/java/com/beautifulyomin/mmmm/domain/stock/service/TradeServiceImpl.java
@@ -31,7 +31,7 @@ public class TradeServiceImpl implements TradeService {
     public void createTrade(TradeDto tradeDto, String userId) {
         // stock 정보 조회
         Stock stock = stockRepository.findById(tradeDto.getStockCode())
-                .orElseThrow(() -> new IllegalArgumentException("Invalid stock code"));
+                .orElseThrow(() -> new IllegalArgumentException("Invalid stock code. 주식코드가 유효하지 않습니다."));
 
         // userId로 아이 정보 조회해서 dto에 childrenId 설정하기
         Children children = childrenRepository.findByUserId(userId)
@@ -45,7 +45,8 @@ public class TradeServiceImpl implements TradeService {
 
         if (stocksHeld == null && tradeDto.getTradeType().equals("5")) {
             // 매도 요청인데 stocksHeld가 없으면 에러 발생
-            throw new IllegalArgumentException("Cannot sell stock that is not held.");
+            log.error("매도 주수 없음 에러");
+            throw new IllegalArgumentException("Cannot sell stock that is not held. 매도할 수 있는 보유주수가 없습니다.");
         }
 
         // stocksHeld가 없고 매수인 경우에만 새로운 StocksHeld 생성
@@ -62,7 +63,7 @@ public class TradeServiceImpl implements TradeService {
         if (tradeDto.getTradeType().equals("4")) { // 매수
             // 매수 시 잔액 부족 여부 체크
             if (children.getMoney() < tradeDto.getAmount()) {
-                throw new IllegalArgumentException("Insufficient funds for this purchase");
+                throw new IllegalArgumentException("Insufficient funds for this purchase. 매수하기에 머니가 부족합니다.");
             }
             totalProfit = BigDecimal.ZERO;
             handleBuyTransaction(stocksHeld, tradeDto);
@@ -74,7 +75,7 @@ public class TradeServiceImpl implements TradeService {
             log.info("totalProfit = {}", totalProfit);
             children.setMoney(children.getMoney() + tradeDto.getAmount() + totalProfit.intValue());
         } else {
-            throw new IllegalArgumentException("Invalid trade type");
+            throw new IllegalArgumentException("Invalid trade type. 매매 타입이 유효하지 않습니다.");
         }
 
         // StocksHeld 저장
@@ -110,7 +111,8 @@ public class TradeServiceImpl implements TradeService {
     // 매도 처리
     private BigDecimal handleSellTransaction(StocksHeld stocksHeld, TradeDto tradeDto) {
         if (stocksHeld.getRemainSharesCount().compareTo(tradeDto.getTradeSharesCount()) < 0) {
-            throw new IllegalArgumentException("Not enough shares to sell");
+            log.debug("Not enough shares to sell");
+            throw new IllegalArgumentException("Not enough shares to sell. 매도하기에 돈이 충분하지 않습니다.");
         }
 
         //평단가 계산
