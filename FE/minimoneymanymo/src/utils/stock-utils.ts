1b0dc4e4017c043405774bebfdcc5f43f68f99a2
김세민 <2007ksm@naver.com>
2024-09-27 14:07:59 +0900

    fix: 시가총액 단위 수정

--- FE/minimoneymanymo/src/utils/stock-utils.ts
+++ FE/minimoneymanymo/src/utils/stock-utils.ts
@@ -10,10 +10,9 @@ export const getPriceChangeColorAndSign = (value: number) => {
 
 // 시가총액 형식 변환 함수
 export const formatMarketCapitalization = (value: number): string => {
-  if (value >= 100000000) {
-    return `${(value / 100000000).toFixed(2)} 조원`
-  } else if (value >= 10000) {
-    return `${(value / 10000).toFixed(2)} 억원`
+  if (value >= 10000) {
+    return `${(value / 10000).toFixed(2)} 조원`
+  } else {
+    return `${value.toFixed(2)} 억원`
   }
-  return `${value.toLocaleString()} 만원`
 }
