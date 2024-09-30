1803005283ebf30b9fc5d54d53b713b0bc4aea27
이창현 <96161882+changhyeon12333@users.noreply.github.com>
2024-09-30 12:40:30 +0900

    fix : quiz time

--- BE/mmmmBatch/src/main/java/com/beautifulyomin/mmmmbatch/runner/QuizJobRunner.java
+++ BE/mmmmBatch/src/main/java/com/beautifulyomin/mmmmbatch/runner/QuizJobRunner.java
@@ -11,7 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class QuizJobRunner implements CommandLineRunner { //
+public class QuizJobRunner  { //implements CommandLineRunner
     private final JobLauncher jobLauncher;
     private final Job newsQuizJob;
 
@@ -22,18 +22,18 @@ public class QuizJobRunner implements CommandLineRunner { //
 
     }
 
-//    @Scheduled(cron = "0 0 0 ? * MON-FRI")
-//    public void run() throws Exception {
-//        JobParameters params = new JobParametersBuilder()
-//                .addLong("time", System.currentTimeMillis()) // JobParameters에 현재 시간 추가
-//                .toJobParameters();
-//        jobLauncher.run(webCrawlingJob, params); // 잡 실행
-//    }
-    @Override
-    public void run(String... args) throws Exception {
+    @Scheduled(cron = "0 43 12 ? * MON-FRI")
+    public void run() throws Exception {
         JobParameters params = new JobParametersBuilder()
-                .addLong("time", System.currentTimeMillis())
+                .addLong("time", System.currentTimeMillis()) // JobParameters에 현재 시간 추가
                 .toJobParameters();
-        jobLauncher.run(newsQuizJob, params); // 애플리케이션 시작 시 배치 작업 실행
+        jobLauncher.run(newsQuizJob, params); // 잡 실행
     }
+//    @Override
+//    public void run(String... args) throws Exception {
+//        JobParameters params = new JobParametersBuilder()
+//                .addLong("time", System.currentTimeMillis())
+//                .toJobParameters();
+//        jobLauncher.run(newsQuizJob, params); // 애플리케이션 시작 시 배치 작업 실행
+//    }
 }
