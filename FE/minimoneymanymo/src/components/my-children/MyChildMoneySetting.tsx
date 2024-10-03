2db23bf170b48027bf3f889dc24e93ec01ccd331
이다영 <da0997@naver.com>
2024-10-03 17:14:25 +0900

    Revert "feat: 부모 잔액 적용( 임시저장)"
    This reverts commit 83a72cb996fbc9d15b2b9acb6508a57b13647a14.

--- FE/minimoneymanymo/src/components/my-children/MyChildMoneySetting.tsx
+++ FE/minimoneymanymo/src/components/my-children/MyChildMoneySetting.tsx
@@ -7,9 +7,6 @@ import {
   updateWithdrawableMoney,
 } from "@/api/user-api"
 import Heading from "../common/Heading"
-import { useAppDispatch, useAppSelector } from "@/store/hooks"
-import { selectParent } from "@/store/slice/parent"
-import { setMemberInfo } from "@/utils/user-utils"
 
 function MyChildMoneySetting(): JSX.Element {
   const { child } = useChild()
@@ -18,21 +15,19 @@ function MyChildMoneySetting(): JSX.Element {
   const [withdrawableMoney, setWithdrawableMoney] = useState<number | null>(
     null
   )
-  const [maxWithdrawableMoney, setMaxWithdrawableMoney] = useState<
+  const [maxWithdrawableMoney, setmaxWithdrawableMoney] = useState<
     number | null
   >(null)
   const [quizBonusMoney, setQuizBonusMoney] = useState<number | null>(null)
   const [selectedMenu, setSelectedMenu] = useState<string>("allowance")
-  const [inputValue, setInputValue] = useState<number | undefined>(undefined)
+  const [inputValue, setInputValue] = useState<number | "">("")
   const [error, setError] = useState<string | null>(null)
-  const parent = useAppSelector(selectParent)
-  const dispatch = useAppDispatch()
 
   const getMyChildsetting = () => {
     setAllowance(child?.settingMoney || null)
     setWithdrawableMoney(child?.settingWithdrawableMoney || null)
     setQuizBonusMoney(child?.settingQuizBonusMoney || null)
-    setMaxWithdrawableMoney(parent.balance) //부모의 잔액 balance - 다른 자식의 최대출금가능금액 뺀값
+    setmaxWithdrawableMoney(100000) //부모의 잔액 balance - 다른 자식의 최대출금가능금액 뺀값
   }
 
   useEffect(() => {
@@ -40,10 +35,6 @@ function MyChildMoneySetting(): JSX.Element {
       getMyChildsetting()
     }
   }, [child])
-
-  useEffect(() => {
-    setMaxWithdrawableMoney(parent.balance)
-  }, [withdrawableMoney])
 
   if (!child) return <div>Child data not available</div>
 
@@ -69,7 +60,7 @@ function MyChildMoneySetting(): JSX.Element {
       }
       const res = await updateAllowance(child.childrenId, inputValue)
       if (res.stateCode === 201) {
-        setAllowance(inputValue === undefined ? null : inputValue)
+        setAllowance(inputValue === "" ? null : inputValue)
         console.log("용돈이 성공적으로 업데이트 되었습니다:", res)
       } else if (res.status === 403) {
         console.error("로그인이 필요합니다.", res)
@@ -81,7 +72,6 @@ function MyChildMoneySetting(): JSX.Element {
       }
     }
     //출금가능금액
-    //이거 정할때마다 balance에서 차감 되어야함.
     else if (selectedMenu === "withdrawableMoney") {
       if (maxWithdrawableMoney && value > maxWithdrawableMoney) {
         setError("입력 값이 마니모 계좌의 범위를 벗어났습니다.")
@@ -89,9 +79,8 @@ function MyChildMoneySetting(): JSX.Element {
       }
       const res = await updateWithdrawableMoney(child.childrenId, inputValue)
       if (res.stateCode === 201) {
-        setWithdrawableMoney(inputValue === undefined ? null : inputValue)
+        setWithdrawableMoney(inputValue === "" ? null : inputValue)
         console.log("withdrawableMoney 성공적으로 업데이트 되었습니다:", res)
-        setMemberInfo(dispatch, 0)
       } else if (res.status === 403) {
         console.error("로그인이 필요합니다.", res)
         // 로그인 페이지로 리다이렉트
@@ -106,7 +95,7 @@ function MyChildMoneySetting(): JSX.Element {
     else if (selectedMenu === "quizBonusMoney") {
       const res = await updateQuizBonusMoney(child.childrenId, inputValue)
       if (res.stateCode === 201) {
-        setQuizBonusMoney(inputValue === undefined ? null : inputValue)
+        setQuizBonusMoney(inputValue === "" ? null : inputValue)
 
         console.log("quizBonusMoney 성공적으로 업데이트 되었습니다:", res)
       } else if (res.status === 403) {
@@ -193,9 +182,7 @@ function MyChildMoneySetting(): JSX.Element {
                 type="number"
                 value={inputValue}
                 onChange={(e) =>
-                  setInputValue(
-                    e.target.value ? Number(e.target.value) : undefined
-                  )
+                  setInputValue(e.target.value ? Number(e.target.value) : "")
                 }
                 className="border-b border-gray-400 text-right"
               />
