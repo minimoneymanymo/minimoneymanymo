ddd0379ffda81fbd8f1d5a177bbe081b7ffbd0fb
조아름 <a_lme@naver.com>
2024-10-05 18:02:57 +0900

    feat: 기록 부분 일부 수정

--- FE/minimoneymanymo/src/components/my-children/MyChildDiaryGiveMoney.tsx
+++ FE/minimoneymanymo/src/components/my-children/MyChildDiaryGiveMoney.tsx
@@ -21,6 +21,16 @@ const MyChildDiaryGiveMoney: React.FC<MyChildDiaryGiveMoneyProps> = ({
   const tradeTypeDisplay =
     diary.tradeType === "4" ? "매수" : diary.tradeType === "5" ? "매도" : ""
 
+  const formatDate = (dateString: string) => {
+    const year = dateString.substring(0, 4) // 2024
+    const month = dateString.substring(4, 6) // 06
+    const day = dateString.substring(6, 8) // 28
+    const hours = dateString.substring(8, 10) // 14
+    const minutes = dateString.substring(10, 12) // 11
+
+    return `${year}년 ${parseInt(month, 10)}월 ${parseInt(day, 10)}일 ${hours}:${minutes}`
+  }
+
   // 보너스 머니 전송 함수
   const handleSendBonusMoney = async () => {
     // child가 null인지 확인
@@ -58,28 +68,54 @@ const MyChildDiaryGiveMoney: React.FC<MyChildDiaryGiveMoneyProps> = ({
 
   return (
     <div>
-      <p>
-        {diary.companyName} {tradeTypeDisplay} <br />
+      <p className="text-lg">
+        {tradeTypeDisplay} <br />
       </p>
-      <p>{diary.amount}원</p>
-      <p>일시 : {diary.createdAt}</p>
-      <p>
+      <p
+        className={`text-xl font-bold ${tradeTypeDisplay === "매수" ? "text-buy" : "text-sell"}`}
+      >
+        {tradeTypeDisplay === "매수" ? "+" : "-"}
+        {diary.amount.toLocaleString()} 머니
+      </p>
+      <p className="p-2"></p>
+      <div className="flex w-full justify-between text-gray-500">
+        <p>거래 유형 </p>
+        <p>
+          {diary.companyName} {tradeTypeDisplay}
+        </p>
+      </div>
+      <div className="flex w-full justify-between text-gray-500">
+        <p>일시</p>
+        <p>{formatDate(diary.createdAt)}</p>
+      </div>
+      <br />
+      <p className="text-gray-500">
         이유
-        <br /> {diary.reason}
+        <br />
       </p>
+      <p className="p-2">{diary.reason}</p>
+      <br />
+      {/* 머니 지급 칸 출력 */}
+      {/* 머니 지급 칸 출력 */}
+      {/* 머니 지급 칸 출력 */}
       {!transactionCompleted &&
         diary.reasonBonusMoney === null && ( // 거래가 완료되지 않았고 reasonBonusMoney가 null인 경우에만 표시
-          <div>
-            <label>
-              이유 머니 입력:
-              <input
-                type="text"
-                value={bonusMoney}
-                onChange={(e) => setBonusMoney(e.target.value)} // 입력값 상태 업데이트
-              />
-            </label>
-            <button onClick={handleSendBonusMoney}>이유 머니 지급하기</button>{" "}
+          <div className="flex flex-col items-center">
+            <input
+              type="text"
+              value={bonusMoney}
+              onChange={(e) => setBonusMoney(e.target.value)} // 입력값 상태 업데이트
+              placeholder="지급 머니를 입력해주세요" // 플레이스홀더 텍스트
+              className="w-30 w-fit rounded-lg bg-gray-300 p-2 text-center text-white placeholder-white" // 클래스 적용
+            />
             {/* 버튼 클릭 시 API 호출 */}
+            <p className="p-1"></p>
+            <button
+              className="w-20 rounded-lg bg-secondary-600-m2 p-2 text-center text-white"
+              onClick={handleSendBonusMoney}
+            >
+              지급
+            </button>{" "}
           </div>
         )}
       {message && <p>{message}</p>} {/* 처리 결과 메시지 표시 */}
