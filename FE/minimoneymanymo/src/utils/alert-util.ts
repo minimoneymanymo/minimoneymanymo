f89502776b9fb9ddfcc177fbe09ee34705f87df0
이다영 <da0997@naver.com>
2024-10-08 15:22:36 +0900

    fix: 주식 좋아요 기능 수정

--- FE/minimoneymanymo/src/utils/alert-util.ts
+++ FE/minimoneymanymo/src/utils/alert-util.ts
@@ -1,9 +1,9 @@
 import Swal from "sweetalert2"
 
 export const alertBasic = (icon: string, content: string) => {
-  const html = `<div class="flex flex-col justify-center space-y-5">
-        ${icon}
-        <span class="text-xl">${content} </span>
+  const html = `<div class="flex flex-col justify-center items-center my-8 space-y-5">
+        <img class="size-32" src="/images/${icon}" alt="error"/>
+        <span class="text-xl font-bold">${content} </span>
       </div>`
   // AlertProps 타입을 사용
   Swal.fire({
@@ -13,6 +13,21 @@ export const alertBasic = (icon: string, content: string) => {
     showConfirmButton: false,
     timer: 1500,
     // icon: "error",
+    customClass: {
+      title: "text-xl", // Tailwind로 title에 작은 글씨 크기 적용
+      popup: "p-4", // 전체 팝업에 패딩 추가 (선택 사항)
+    },
+  })
+}
+export const alertWarning = (content: string) => {
+  // AlertProps 타입을 사용
+  Swal.fire({
+    position: "center",
+    text: content,
+    // title: "아이디, 비밀번호가 일치하지 않습니다",
+    showConfirmButton: false,
+    timer: 1500,
+    icon: "error",
     customClass: {
       title: "text-xl", // Tailwind로 title에 작은 글씨 크기 적용
       popup: "p-4", // 전체 팝업에 패딩 추가 (선택 사항)
