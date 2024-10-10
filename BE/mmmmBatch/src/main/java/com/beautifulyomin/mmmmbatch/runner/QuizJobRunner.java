5a302857c12d2db617782967931feae13b893ea3
김세민 <2007ksm@naver.com>
2024-10-10 09:44:54 +0900

    fix: 퀴즈 스케쥴러 시간 변경

--- BE/mmmmBatch/src/main/java/com/beautifulyomin/mmmmbatch/runner/QuizJobRunner.java
+++ BE/mmmmBatch/src/main/java/com/beautifulyomin/mmmmbatch/runner/QuizJobRunner.java
@@ -11,7 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class QuizJobRunner  implements  CommandLineRunner{ //
+public class QuizJobRunner { //  implements  CommandLineRunner
     private final JobLauncher jobLauncher;
     private final Job newsQuizJob;
 
@@ -30,11 +30,11 @@ public class QuizJobRunner  implements  CommandLineRunner{ //
         jobLauncher.run(newsQuizJob, params); // 잡 실행
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        JobParameters params = new JobParametersBuilder()
-                .addLong("time", System.currentTimeMillis())
-                .toJobParameters();
-        jobLauncher.run(newsQuizJob, params); // 애플리케이션 시작 시 배치 작업 실행
-    }
+//    @Override
+//    public void run(String... args) throws Exception {
+//        JobParameters params = new JobParametersBuilder()
+//                .addLong("time", System.currentTimeMillis())
+//                .toJobParameters();
+//        jobLauncher.run(newsQuizJob, params); // 애플리케이션 시작 시 배치 작업 실행
+//    }
 }
